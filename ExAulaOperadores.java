c33e1065e94f9331b9504b51b7fc7d59cc7b2c1f
julio carvalho <feijucarvalho@gmail.com>
2021-08-28 13:22:23 -0300

    Exercicios

---
+++ ExAulaOperadores.java
@@ -0,0 +1,47 @@
+package cwi.reset.aula_operadores;
+
+import java.util.Scanner;
+
+public class ExAulaOperadores {
+
+	public static void main(String[] args) {
+		
+		/*Faça um programa que tenha como entradas dois números e imprima a soma de ambos. Utilize a classe Scanner para obter as entradas.
+Faça um programa que tenha como entradas 4 notas e imprima a média. Utilize a classe Scanner para obter as entradas.
+Faça um programa que solicite quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.*/
+		
+		Scanner scan = new Scanner(System.in);
+		
+		/* Exercicio 1
+		System.out.println("Digite dois numeros: ");
+		int numero1 = scan.nextInt();
+		int numero2 = scan.nextInt();
+		System.out.println(numero1 +numero2);
+		*/
+		
+		/*Exercicio 2
+		System.out.println("Digite as suas 4 notas");
+		double nota1 = scan.nextDouble();
+		double nota2 = scan.nextDouble();
+		double nota3 = scan.nextDouble();
+		double nota4 = scan.nextDouble();
+		System.out.println((nota1+nota2+nota3+nota4)/4);
+		*/
+		
+		/* Exercicio 3
+		System.out.println("Digite o valor que voce recebe por hora e a quantidade de horas trabalhadas no mes");
+		double valorHora = scan.nextDouble();
+		double horasTrabalhadas = scan.nextDouble();
+		System.out.println("Seu salario no referido mes foi de: " + (valorHora * horasTrabalhadas));
+		*/
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
