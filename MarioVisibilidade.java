86c132bb6a2bbbfb25f112291409aba7916ab9cd
Feijucarvalho <88805456+Feijucarvalho@users.noreply.github.com>
2021-09-01 20:42:00 -0300

    Update MarioVisibilidade.java

--- MarioVisibilidade.java
+++ MarioVisibilidade.java
@@ -16,4 +16,25 @@ public class MarioVisibilidade {
             System.out.println(poderes);
         }
     }
+//Fim 
+//inicio main
+
+  
+public class MarioVisibilidadeMain {
+
+    public static void main(String[] args) {
+
+                MarioVisibilidade mario = new MarioVisibilidade();
+                mario.nacionalidade = "Italiano";
+                mario.corCabelo = "Castanho";
+                //mario.corOlhos = "Azuis";
+                mario.tamanho = "Baixa";
+                mario.poderes = "Ele pode voar, pula alto, derruba objeto com o peso do seu corpo, etc...";
+                mario.caracetristicasFisicas();
+                mario.superPoderes();
+
+
+            }
+        }
+
 
