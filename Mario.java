418de94d9c3155138a0a47ebc45c9ddf0f9fb13d
Feijucarvalho <88805456+Feijucarvalho@users.noreply.github.com>
2021-09-02 19:51:57 -0300

    Update Mario.java

--- Mario.java
+++ Mario.java
@@ -76,3 +76,21 @@ public class Mario {
     }
 }
 
+//fim da classe - Inicio main
+
+  
+public class Main {
+    public static void main(String[] args) {
+
+        Mario m1 = new Mario();
+        m1.superMario();
+        m1.superPoderes();
+
+        System.out.println();
+
+        Mario m2 = new Mario("Super Mario",40,1.50);
+        m2.superMario();
+        m2.superPoderes();
+    }
+}
+
