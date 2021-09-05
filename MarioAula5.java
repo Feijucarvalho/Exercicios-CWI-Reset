62c50e58e7a966354faa5255e7578c48dfd72364
Feijucarvalho <88805456+Feijucarvalho@users.noreply.github.com>
2021-09-05 08:58:37 -0300

    Add files via upload

---
+++ MarioAula5.java
@@ -0,0 +1,92 @@
+public class MarioAula5 {
+
+
+
+    private String nacionalidade;
+    private String corOlhos;
+    private String tamanho;
+    private String corCabelo;
+    private String poderes;
+    private int stamina;
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public MarioAula5(){
+        this.nome = "Mario Bros";
+        this.idade = 40;
+        this.altura = 1.50;
+        this.stamina = 100;
+    }
+
+    public MarioAula5(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.stamina = 100;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void perderStamina(){
+        this.stamina = this.stamina - 10;
+        if (this.stamina < 0){
+            this.stamina = 0;
+        }
+    }
+
+    public void ganharStamina(){
+        this.stamina = this.stamina +5;
+        if (this.stamina>100){
+            this.stamina = 100;
+        }
+    }
+
+    public void superMario () {
+        System.out.println("Nome: "+this.nome+", Idade: "+this.idade+", Altura: "+this.altura+", Stamina: "+this.stamina);
+
+    }
+
+    public void superPoderes (){
+        System.out.println("Poder dar grandes saltos, mata inimigos com o peso do seu corpo, pode até voar");
+    }
+
+}
