a298ca1d31614ade15048a1c7541f929b99cf773
Feijucarvalho <88805456+Feijucarvalho@users.noreply.github.com>
2021-09-06 11:34:38 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -129,3 +129,23 @@ public class Personagem implements Controlador {
 
 }
 
+// fim classe Personagem - Inicio classe main
+
+public class MarioEncapsulamento {
+        public static void main(String[] args) {
+
+            Personagem mario = new Personagem("Mario",40,1.5);
+            Personagem luigui = new Personagem("Luigui",35,1.75);
+
+            mario.superMario();
+            mario.superPoderes();
+
+            luigui.superMario();
+            luigui.superPoderes();
+
+            System.out.println();
+
+
+        }
+    }
+
