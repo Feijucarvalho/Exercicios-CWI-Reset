9de24d1d226d42a3de7c7f096c5b3e458e985c77
Feijucarvalho <88805456+Feijucarvalho@users.noreply.github.com>
2021-09-08 19:20:35 -0300

    Update MarioMain.java

--- MarioMain.java
+++ MarioMain.java
@@ -147,3 +147,29 @@ public abstract class Personagem {
 
 
 }
+
+// Fim da classe Personagem - Inicio da classe Mario
+
+public class Mario extends Personagem{
+    private int yoshi;
+
+    public int getYoshi() {
+        return yoshi;
+    }
+
+    public void setYoshi(int yoshi) {
+        this.yoshi = yoshi;
+    }
+
+    public void montarNoYoshi(){
+        yoshi =+1;
+    }
+    public void marioMontadoNoYoshi(){
+        if (this.yoshi == 0){
+            System.out.println("Mario nao está montado no yoshi");
+        }else{
+            System.out.println("Mario esta montado no Yoshi");
+        }
+    }
+
+}
